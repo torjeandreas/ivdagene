aeff7f8ef1c332cb2ed4c3337bd68d6a05872adb
Torje <torje.rolfsen@gmail.com>
2021-07-09 09:51:51 +0200

    more skeleton files and code

--- ivdagene1/urls.py
+++ ivdagene1/urls.py
@@ -1,4 +1,4 @@
-"""ivdagene URL Configuration
+"""ivdagene1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from main import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name="index"),
 ]
